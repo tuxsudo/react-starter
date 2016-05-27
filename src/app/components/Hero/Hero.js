643fb852468912ea3edd448aa6e5a5caf86c851e
Jared Anderson <jared@tuxsudo.com>
2016-05-27 15:59:44 -0600

    Add <Hero> component
    Hero added for demonstration purposes and to be
    used in default welcome message for new projects.

---
+++ src/app/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import styles from './style.css';
+import classes from 'classnames';
+
+export const Hero = ({title, subtitle, bgSrc, overlay=false, href, onClick, className}) => (
+    <a
+        className={classes(styles.hero, className, (href || onClick) && styles.clickable)}
+        style={{backgroundImage: bgSrc && `url(${bgSrc})`||""}}
+        onClick={onClick}
+        {...(href ? { href } : {})}
+    >
+
+        {overlay && <span className={classes(styles.overlay, styles[overlay] )} />}
+
+        <h1 className={styles.title}>
+            {title}
+            {subtitle && <small className={styles.subtitle}>{subtitle}</small>}
+        </h1>
+
+
+    </a>
+);
+
+
+export default Hero;
---
+++ src/app/components/Hero/HeroStory.js
@@ -0,0 +1,70 @@
+import { storiesOf, action } from '@kadira/storybook';
+import Hero from './Hero';
+import imgSrc from './slc.jpg';
+
+
+storiesOf('Hero', module)
+
+    .add('basic', () => (<Hero title="Welcome to the Hero Component" />))
+
+    .add('subtitled', () => (
+        <Hero
+            title="Welcome to the Hero Component"
+            subtitle="this one has a subtitle"
+        />
+    ))
+
+    .add('background', () => (
+        <Hero
+            title="Welcome to the Hero Component"
+            subtitle="this one has a subtitle and image"
+            bgSrc={imgSrc}
+        />
+    ))
+
+    .add('overlay-light', () => (
+        <Hero
+            title="Welcome to the Hero Component"
+            subtitle="this one has a subtitle and image and overlay"
+            bgSrc={imgSrc}
+            overlay="light"
+        />
+    ))
+
+    .add('overlay', () => (
+        <Hero
+            title="Welcome to the Hero Component"
+            subtitle="this one has a subtitle and image and overlay"
+            bgSrc={imgSrc}
+            overlay
+        />
+    ))
+
+    .add('overlay-dark', () => (
+        <Hero
+            title="Welcome to the Hero Component"
+            subtitle="this one has a subtitle and image and overlay"
+            bgSrc={imgSrc}
+            overlay="dark"
+        />
+    ))
+
+    .add('onClick', () => (
+        <Hero
+            title="Welcome to the Hero Component"
+            subtitle="this one has a subtitle and image and overlay and action (click me)"
+            bgSrc={imgSrc}
+            overlay="dark"
+            onClick={action('clicked action')}
+        />
+    ))
+
+    .add('href', () => (
+        <Hero
+            title="Welcome to the Hero Component"
+            subtitle="this one has a subtitle and image and overlay and action (click me)"
+            bgSrc={imgSrc}
+            overlay="dark"
+            href="//example.org"
+        />
+    ));
