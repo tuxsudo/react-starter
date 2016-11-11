740a53ed7e2c4e3b0822c6e7bf39f95eb7c11919
Jared Anderson <jared@tuxsudo.com>
2016-11-10 22:27:23 -0700

    Allow toggling of redux dev tools by env variable
    By default, redux dev tools will be available but it can be
    toggle off by setting the environment variable ALLOW_REDUX_DEV_TOOLS
    = 0

--- src/app/env.js
+++ src/app/env.js
@@ -3,5 +3,6 @@ import isBrowser from 'is-in-browser';
 //Grab variables from process.env or window
 export const {
     APP_WEB_BASE_PATH,
-    API_HOST
+    API_HOST,
+    ALLOW_REDUX_DEV_TOOLS
 } = isBrowser ? window.__APP_ENV_VARS__ : process.env;
--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,8 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import isBrowser from 'is-in-browser';
 
+import {ALLOW_REDUX_DEV_TOOLS} from './env.js';
+
 import * as reducers from './reducers';
 
 // create the master reducer
@@ -14,7 +16,7 @@ const initialState = isBrowser && window.__INITIAL_STATE__ || {};
 
 const reduxMiddleware = compose(
     applyMiddleware(thunk),
-    isBrowser && typeof window.devToolsExtension !== "undefined" ? window.devToolsExtension() : f => f
+    isBrowser && ALLOW_REDUX_DEV_TOOLS==="1" && typeof window.devToolsExtension !== "undefined" ? window.devToolsExtension() : f => f
 );
 
 // export a store creator factory with initial state if present...
