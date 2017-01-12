7ea48fcc9e825230bed48eaac78cb209a99e5107
Jared Anderson <jared@tuxsudo.com>
2017-01-11 17:02:14 -0700

    Change bundling whitelist to a blacklist instead
    When building the server, we want to exclude node-only
    modules from the application bundle. Before, everything was
    excluded unless it was added to whitelist. Changed that to a blacklist
    and seeded blacklist with files that shouldn't be bundled.

--- webpack.server.babel.js
+++ webpack.server.babel.js
@@ -1,6 +1,5 @@
 import base from './webpack.base.babel.js';
 import path from 'path';
-import nodeExternals from 'webpack-node-externals';
 
 const {APP_WEB_BASE_PATH} = process.env;
 
@@ -16,9 +15,27 @@ export default {
         publicPath: `${APP_WEB_BASE_PATH}/`
     },
 
-    externals: [ nodeExternals({
-        whitelist: ['normalize.css']
-    })],
+    externals: [
+        "better-npm-run",
+        "compression",
+        "cors",
+        "dotenv",
+        "express",
+        "helmet",
+        "html-minifier",
+        "is-in-browser",
+        "isomorphic-fetch",
+        "join-classnames",
+        "npm-run-all",
+        "react",
+        "react-dom",
+        "react-helmet",
+        "react-redux",
+        "react-router",
+        "redux",
+        "redux-thunk",
+        "string-hash"
+    ].reduce((obj, name) => ({...obj, [name]: `commonjs ${name}`})),
 
     target: 'node',
 
