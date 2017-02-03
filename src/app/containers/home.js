ebe78aa42bb32cfda8530beed61b66b4ab9393f9
Jared Anderson <jared@tuxsudo.com>
2017-02-02 21:56:48 -0700

    Remove HoC pattern from container; add meta support
    To improve ability to understand containers, removed abstaction
    of HoC patterns in container files.
    
    Add support to read/write to page meta in store at each container.

--- src/app/containers/home.js
+++ src/app/containers/home.js
@@ -1,18 +1,41 @@
+import {Component} from 'react';
 import Home from '../components/Home';
 import { connect } from 'react-redux';
-import { addMeta } from '../hocs/add-meta';
+import { setPageMeta } from '../actions/page-meta';
 
-const metaHome = addMeta(Home);
-
-const mapStateToProps = () => ({
-    meta: {
-        title: "Homepage, yo!",
-        tags: [
-            {"name": "description", "content": "A React Starter"},
-            {"property": "og:type", "content": "article"}
-        ]
-    }
-});
+const pageMeta = {
+  title: "Homepage, yo!!",
+  tags: [
+      {"name": "description", "content": "A React Starter"},
+      {"property": "og:type", "content": "article"}
+  ]
+};
 
 
-export default connect(mapStateToProps)(metaHome);
+const storeConnector = connect(
+  undefined,
+  {setPageMeta},
+  (store, actions) => ({
+    init: () => actions.setPageMeta(pageMeta)
+  })
+);
+
+
+
+class HomeContainer extends Component {
+
+  static onServer(props, store) {
+    return store.dispatch(setPageMeta(pageMeta))
+  }
+
+  componentDidMount() {
+    this.props.init();
+  }
+
+  render() {
+    return <Home {...this.props} />
+  }
+
+}
+
+export default storeConnector(HomeContainer);
