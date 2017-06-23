a04860491d9581ebd24131fdb2e712a8413a182a
Bruce Campbell <campbellbd@Bruces-MacBook-Pro-2.local>
2017-06-23 09:52:18 -0600

    Use `file-loader` in webpack test config to bundle images
    When using null-loader we get `Error: Cannot find module...`

--- .tests/webpack.js
+++ .tests/webpack.js
@@ -40,7 +40,7 @@ module.exports = {
 
             {
                 test: /\.(png|jpe?g|gif|svg|mp3|mpe?g)$/,
-                loader: "null-loader?name=static/assets/[name]-[hash:2].[ext]"
+                loader: "file-loader?name=static/assets/[name]-[hash:2].[ext]"
             }
 
         ]
